7a8ac7d5e45632a1df607f21a065326daae71350
Sibel Demirel <sibeldmrl19@gmail.com>
2023-01-31 15:08:45 +0100

    Js beginning

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // document.querySelector('.exo1 p:first-child span').innerHTML = students;
 
+let order = document.querySelector('.order');
+let firstSection = document.querySelector('.sec1');
+
+
+order.addEventListener('click', () =>
+    firstSection.innerHTML='Votre commande a été prise en compte',
+    firstSection.classList.add('.green') //doesn't work
+)
 
 
 
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/_GuOjXYl5ew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
 
+// let button = document.querySelector('.lien');
+// let image = document.querySelector('.rightImage');
+// let video = '<iframe width="560" height="315" src="https://www.youtube.com/embed/_GuOjXYl5ew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
 
+// console.log(image)
+// console.log(button)
 
-
-
-
+// button.addEventListener('click', () => 
+//     image.innerHTML = {video}
+// )
