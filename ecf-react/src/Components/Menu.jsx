10ac1c7ae7b5d4a7c254c9235853c4d5bd1d91ce
Sibel Demirel <sibeldmrl19@gmail.com>
2023-02-02 16:50:54 +0100

    star add

--- ecf-react/src/Components/Menu.jsx
+++ ecf-react/src/Components/Menu.jsx
@@ -2,24 +2,32 @@ import meals from "../meals";
 
 
 function Menu() {
-    
 
-    return(
-        <div className="meal">
-            <article>
-                {meals.map((meal,index)=>
 
-                <div key={index} className="card">
+    return (
+        <div className="grid">
+
+            {meals.map((meal, index) =>
+
+                <article key={index} className="card">
 
                     <img src={meal.imageSrc} alt="meal"></img>
-                    <h3>{meal.title}</h3>
-                    <p>{meal.content}</p>
-                    <p>{meal.price}</p>
-                    
-                </div>
-                )}
-            </article>
-    </div>
+                        <h3 className="mealTitle">{meal.title}</h3>
+
+                        <p className="content">{meal.content}</p>
+
+                        <h4 className="price">{meal.price}</h4>
+
+                    <div className="star">
+                    <svg fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5T1385 1619q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5T365 1569q0-6 2-20l86-500L89 695q-25-27-25-48 0-37 56-46l502-73L847 73q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path></svg>
+                    <h6>{meal.rating}</h6>
+                    <p>{meal.reviews}</p>
+                    </div>
+
+                </article>
+            )}
+
+        </div>
     )
 }
 
