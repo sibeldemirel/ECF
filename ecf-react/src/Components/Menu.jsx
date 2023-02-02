691ad3f1eab60dfe6e22a5436d618de42b45f17c
Sibel Demirel <sibeldmrl19@gmail.com>
2023-02-03 00:53:55 +0100

    single

--- ecf-react/src/Components/Menu.jsx
+++ ecf-react/src/Components/Menu.jsx
@@ -1,34 +1,40 @@
+import { useState } from "react";
 import meals from "../meals";
 
 
 function Menu() {
+    let [selectedItem, setSelectedItem] = useState(false);
 
 
     return (
         <div className="grid">
 
-            {meals.map((meal, index) =>
+            {!selectedItem && meals.map((meal, index) =>
 
                 <article key={index} className="card">
 
                     <img src={meal.imageSrc} alt="meal"></img>
-                        <h3 className="mealTitle">{meal.title}</h3>
+                    <h3 className="mealTitle">{meal.title}</h3>
 
-                        <p className="content">{meal.content}</p>
+                    <p className="content">{meal.content}</p>
 
-                        <h4 className="price">{meal.price}</h4>
+                    <h4 className="price">{meal.price}</h4>
 
                     <div className="star">
-                    <svg fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5T1385 1619q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5T365 1569q0-6 2-20l86-500L89 695q-25-27-25-48 0-37 56-46l502-73L847 73q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path></svg> 
-                    <span className="rating">{meal.rating}</span> 
-                    <span className="review">({meal.reviews})</span>
-                    
-                    </div>
-                    <button className="btn">Commander</button>
+                        <svg fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5T1385 1619q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5T365 1569q0-6 2-20l86-500L89 695q-25-27-25-48 0-37 56-46l502-73L847 73q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path></svg>
+                        <span className="rating">{meal.rating}</span>
+                        <span className="review">({meal.reviews})</span>
 
-                    <div></div>
+                    </div>
+                    <button onClick={() => setSelectedItem(meal)}
+                        className="btn">Commander</button>
                 </article>
             )}
+
+            {selectedItem && <div className="single container">
+                <p>Vous avez commandé le reaps {selectedItem.title}</p>
+                <img src={selectedItem.imageSrc} alt="meal" />
+            </div>}
 
         </div>
     )
